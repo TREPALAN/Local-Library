67e94583473db39b97253f1d3bdff7e6acf938ae
alan shaksper <devalanfreitas@gmail.com>
2024-01-16 15:37:09 -0300

    me

--- express-locallibrary-tutorial/models/bookinstance.js
+++ express-locallibrary-tutorial/models/bookinstance.js
@@ -16,6 +16,11 @@ const BookInstanceSchema = new Schema({
 });
 
 // Virtual for bookinstance's URL
+BookInstanceSchema.virtual("url").get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return `/catalog/bookinstance/${this._id}`;
+});
+// Virtual for formatted due back
 BookInstanceSchema.virtual("due_back_formatted").get(function () {
   return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
